01bdd66389e5a27e75912e9d1cc60132b9f26681
Aasimasunar11 <aasimasunar111@gmail.com>
2025-11-10 14:48:32 +0545

    Add project files

---
+++ FirstProgram.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        // ArrayEmployee aa = new ArrayEmployee();
+        // aa.Account();
+        ArrayDimension ad = new ArrayDimension();
+        ad.Array();
+    }
+}
